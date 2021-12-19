56a3eb5edec7b7732969b46a1abb7155586ea741
hatsnake <hatsnake@naver.com>
2021-12-19 16:09:18 +0900

    Add ArrayTest

---
+++ src/ArrayTest/Practice1.java
@@ -0,0 +1,23 @@
+package ArrayTest;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        // 3차원 배열에서 8, 10, 2를 순서대로 각각 라인에 출력
+        // System.out.println() 사용
+        Integer[][][] data_list = {
+            {
+                {1, 2, 3},
+                {4, 5, 6}
+            },
+            {
+                {7, 8, 9},
+                {10, 11, 12}
+            }
+        };
+
+        // 출력
+        System.out.println(data_list[1][0][1]);
+        System.out.println(data_list[1][1][0]);
+        System.out.println(data_list[0][0][1]);
+    }
+}
